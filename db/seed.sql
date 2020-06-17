3ea0e94553b361f83a7f1ec93b9eefb8f9797165
ShelbyCourt <shelby.b.courtney@gmail.com>
2020-06-16 20:08:08 -0700

    Coded Through, but needed de-bugging

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+create table Products
+product_id serial primary key,
+name varchar(40),
+description varchar(80),
+price integer,
+image_url text;
---
+++ db/update_product.sql
@@ -0,0 +1,3 @@
+update Products
+set description = $2
+where product_id = $1;
